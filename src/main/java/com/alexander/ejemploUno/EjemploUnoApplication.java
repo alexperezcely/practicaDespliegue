23e3e1ce2ce31c7d26d3721284db31a266cd38a2
alex2211 <jose.perez.mt@usa.edu.co>
2022-09-20 19:51:37 -0500

    VersionInicial

---
+++ src/main/java/com/alexander/ejemploUno/EjemploUnoApplication.java
@@ -0,0 +1,13 @@
+package com.alexander.ejemploUno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploUnoApplication.class, args);
+	}
+
+}
